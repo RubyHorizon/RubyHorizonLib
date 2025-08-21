f66f19f4f03a0cae236d45c0be9a526b9537e69e
Shershnyaga <leshchukr2004@gmail.com>
2025-08-21 22:24:35 +0300

    Added plugin

---
+++ plugin/src/main/java/team/rubyhorizon/lib/rubyHorizonLib/RubyHorizonLib.java
@@ -0,0 +1,17 @@
+package team.rubyhorizon.lib.rubyHorizonLib;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RubyHorizonLib extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
