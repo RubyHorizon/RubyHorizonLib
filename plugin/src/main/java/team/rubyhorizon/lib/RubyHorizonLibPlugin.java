d568c02b73c231eee71a2db73744aafd5f9a2433
Shershnyaga <leshchukr2004@gmail.com>
2025-08-22 10:22:09 +0300

    Logo ASQII art

--- plugin/src/main/java/team/rubyhorizon/lib/RubyHorizonLibPlugin.java
+++ plugin/src/main/java/team/rubyhorizon/lib/RubyHorizonLibPlugin.java
@@ -1,5 +1,6 @@
 package team.rubyhorizon.lib;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import team.rubyhorizon.lib.metrics.BstatsMetrics;
 
@@ -7,9 +8,24 @@ public final class RubyHorizonLibPlugin extends JavaPlugin {
 
     private BstatsMetrics bstatsMetrics;
 
+    private static final String logoMessage = """
+            \s
+            -----------------------------------
+             _____  _     _      _ _    \s
+            |  __ \\| |   | |    (_) |   \s
+            | |__) | |__ | |     _| |__ \s
+            |  _  /| '_ \\| |    | | '_ \\\s
+            | | \\ \\| | | | |____| | |_) |
+            |_|  \\_\\_| |_|______|_|_.__/\s
+            
+            By Shershnyaga_, Salatosik, Loft69
+            -----------------------------------
+            """;
+
     @Override
     public void onEnable() {
         bstatsMetrics = new BstatsMetrics(this, 27004);
+        Bukkit.getLogger().info(logoMessage);
     }
 
     @Override
